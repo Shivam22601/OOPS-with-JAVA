6e82c4eef80c4691c5c0737d85b9d511e5c92058
Shivam <mishrashivam20197@gmail.com>
2026-04-13 11:55:16 +0530

    hierarhical inheritance

--- Inheritance/Animals.java
+++ Inheritance/Animals.java
@@ -23,6 +23,34 @@
 
 
 //multilevel inheritance
+// class Animal {
+//     void eat() {
+//         System.out.println("Animal eats food");
+//     }
+// }
+
+// class Dog extends Animal {
+//     void bark() {
+//         System.out.println("Dog barks");
+//     }
+// }
+
+// class Puppy extends Dog {
+//     void weep() {
+//         System.out.println("Puppy weeps");
+//     }
+// }
+
+// public class Animals {
+//     public static void main(String[] args) {
+//         Puppy p = new Puppy();
+//         p.eat();   // from Animal
+//         p.bark();  // from Dog
+//         p.weep();  // own
+//     }
+// }
+
+//Hierarchical inheritance
 class Animal {
     void eat() {
         System.out.println("Animal eats food");
@@ -35,17 +63,21 @@ class Dog extends Animal {
     }
 }
 
-class Puppy extends Dog {
-    void weep() {
-        System.out.println("Puppy weeps");
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat meows");
     }
 }
 
 public class Animals {
     public static void main(String[] args) {
-        Puppy p = new Puppy();
-        p.eat();   // from Animal
-        p.bark();  // from Dog
-        p.weep();  // own
+        Dog d = new Dog();
+        Cat c = new Cat();
+
+        d.eat();
+        d.bark();
+
+        c.eat();
+        c.meow();
     }
 }
