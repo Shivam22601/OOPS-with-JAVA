77b5e377fbaef4e4ac72079fc9f63a12761bd4c6
Shivam <mishrashivam20197@gmail.com>
2026-04-13 11:53:50 +0530

    multilevel inheritance

--- Inheritance/Animals.java
+++ Inheritance/Animals.java
@@ -1,3 +1,28 @@
+//Single inheritance
+
+// class Animal {
+//     void eat() {
+//         System.out.println("Animal eats food");
+//     }
+// }
+
+// class Dog extends Animal {
+//     void bark() {
+//         System.out.println("Dog barks");
+//     }
+// }
+
+// public class Animals{
+//     public static void main(String[] args) {
+//         Dog d = new Dog();
+//         d.eat(); // inherited method from Animal class
+//         d.bark(); // method of Dog class
+//     }
+// }
+
+
+
+//multilevel inheritance
 class Animal {
     void eat() {
         System.out.println("Animal eats food");
@@ -10,10 +35,17 @@ class Dog extends Animal {
     }
 }
 
-public class Animals{
+class Puppy extends Dog {
+    void weep() {
+        System.out.println("Puppy weeps");
+    }
+}
+
+public class Animals {
     public static void main(String[] args) {
-        Dog d = new Dog();
-        d.eat(); // inherited method from Animal class
-        d.bark(); // method of Dog class
+        Puppy p = new Puppy();
+        p.eat();   // from Animal
+        p.bark();  // from Dog
+        p.weep();  // own
     }
 }
